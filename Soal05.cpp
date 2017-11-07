75d878d681409dacf29044fb8b142a75de7db85d
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2017-11-07 19:05:41 +0700

    penyelesaian soal05

--- Soal05.cpp
+++ Soal05.cpp
@@ -15,5 +15,13 @@ int main () {
 		cout <<""<<endl;
 		cout << "Anda Tergolong underweight / Kurus"<<endl;
 	}
+	else if ((T/2.3)<B){
+		system("cls");
+		cout << ""<<endl;
+		cout << "Tinggi Anda "<<T<<" Kg"<<endl;
+		cout << "Berat Anda  "<<B<<" Cm"<<endl;
+		cout <<""<<endl;
+		cout << "Anda Tergolong Overweight / Berat"<<endl;
+	}	
 
 }
