2a5fa1b43fd28c69895ad9ec3b512edab971c841
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2017-11-07 09:10:10 +0700

    penyelesaian soal01

--- Soal01.cpp
+++ Soal01.cpp
@@ -7,4 +7,10 @@ int main () {
 	cout << "Masukkan Nilai X : "; cin >> X ;
 	cout << "Masukkan Nilai Y : "; cin >> Y ;
 
+	if (X>Y){
+		Z = X-Y;
+		
+		cout << "Selisih Nilai X dan Y yaitu "<< Z ;
+	}
+
 }
