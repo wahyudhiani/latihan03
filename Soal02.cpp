6b7fdb52da7f550f3cc4222c8adfa40a77755fa5
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2017-11-07 18:53:50 +0700

    penyelesaian soal02

--- Soal02.cpp
+++ Soal02.cpp
@@ -11,5 +11,14 @@ int main () {
 	cout << "Luas yang akan di Hitung P atau S ? "; cin >> Pilih;
 
 	
-
+	if (Pilih=='P'){
+		system ("cls");
+		cout << "Anda Menghitung Luas Persegi Panjang"<<endl ;
+		cout <<""<<endl;
+		cout << "Berapa Panjang Persegi Panjangnya ? "; cin >> Pa ;
+		cout << "Berapa Lebar Persegi Panjangnya   ? "; cin >> Le ;
+		L = Pa*Le ;
+		cout << "Jadi Luas Persegi Panjangnya adalah  "<< L ;
+	}	
+	
 }
