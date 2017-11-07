22cc732bb6591095799827b1ab83131dbd8da0c8
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2017-11-07 19:02:09 +0700

    penyelesaian soal04

--- Soal04.cpp
+++ Soal04.cpp
@@ -11,5 +11,10 @@ int main () {
 		cout << ""<<endl;
 		cout << "Wah...Bilangan yang Anda Masukkan Adalah Positif ( "<<B<<" )!!"<<endl;
 	}
+	else if (B<0){
+		system("cls");
+		cout << ""<<endl;
+		cout << "Oh Tidak...Bilangan yang Anda Masukkan Adalah Negatif ( "<<B<<" )!!"<<endl;
+	}
 	
 }
